497fb64b688ad6de94c8120e475bc712e090e9c7
lennan747 <lennan747@gmail.com>
2019-09-12 23:45:05 +0800

    安装短信服务，添加节流，获取图片验证码

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        App\Providers\EasySmsServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
