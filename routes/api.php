15d7183b86d555b400af461f547765b3c0c2bb7e
lennan747 <lennan747@gmail.com>
2019-09-15 01:08:56 +0800

    用户注册，安装jwt

--- routes/api.php
+++ routes/api.php
@@ -28,14 +28,24 @@ $api->version('v1', [
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
         // 用户注册
-//        $api->post('users', 'UsersController@store')
-//            ->name('api.users.store');
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+        // 用户登录
+        $api->post('authorizations','AuthorizationsController@store')
+            ->name('api.authorizations.store');
+        // 刷新token
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+        // 删除token
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
 
-        $api->post('captchas', 'CaptchasController@store')
-            ->name('api.captchas.store');
     });
 });
