c02643273e41443466a92b3277b860b021aed3eb
lennan747 <lennan747@gmail.com>
2019-10-14 15:43:43 +0800

    用户名称

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -14,7 +14,8 @@ class UserTransformer extends TransformerAbstract
             'phone'         => $user->phone,
             'identity'      => $user->identity,             // 用户身份信息
             'money'         => $user->money,                // 用户余额
-            'avatar'        => $user->avatar,
+            'avatar'        => $user->avatar ? $user->avatar : 'http://mb.jiangzi20.com/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg',
+            'name'          => $user->name,
             // 会员订单信息
             //'member_order'  => $user->order()->where([['user_id','=' ,$user->id], ['type','=', Order::ORDER_TYPE_MEMBER]])->first(),
             'created_at'    => (string) $user->created_at,
